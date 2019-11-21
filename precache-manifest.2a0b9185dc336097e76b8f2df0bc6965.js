039bfb62caa681e397e0ed055440d20e651554df
rakeshkpanda <rakesh.k.panda@gmail.com>
2019-11-21 19:26:56 +0530

    Add files via upload

---
+++ precache-manifest.2a0b9185dc336097e76b8f2df0bc6965.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f81f4abdd1b132e55f2bab492f8a319",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6994d8b115f62abb7993",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "33e93d31dc6c35aede84",
+    "url": "/static/js/2.e77b1c85.chunk.js"
+  },
+  {
+    "revision": "6994d8b115f62abb7993",
+    "url": "/static/js/main.e105bf4f.chunk.js"
+  },
+  {
+    "revision": "aa516e78a44c48a3aaf3",
+    "url": "/static/js/runtime-main.5352e72b.js"
+  }
+]);
